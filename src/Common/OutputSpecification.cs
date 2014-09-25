3426a6af7e0c4e32435a489978437cf040888ed4
Charlie Poole <charliepoole@gmail.com>
2014-09-24 20:16:43 -0700

    Move OutputSpecification.cs to Common folder

--- src/Common/OutputSpecification.cs
+++ src/Common/OutputSpecification.cs
@@ -23,12 +23,20 @@
 
 using System;
 
-namespace NUnit.ConsoleRunner
+namespace NUnit.Common
 {
+    /// <summary>
+    /// OutputSpecification encapsulates a file output path and format
+    /// for use in saving the results of a run.
+    /// </summary>
     public class OutputSpecification
     {
         #region Constructor
 
+        /// <summary>
+        /// Construct an OutputSpecification from an option value.
+        /// </summary>
+        /// <param name="spec">The option value string.</param>
         public OutputSpecification(string spec)
         {
             if (spec == null)
@@ -81,10 +89,19 @@ namespace NUnit.ConsoleRunner
 
         #region Properties
 
+        /// <summary>
+        /// Gets the path to which output will be written
+        /// </summary>
         public string OutputPath { get; private set; }
 
+        /// <summary>
+        /// Gets the name of the format to be used
+        /// </summary>
         public string Format { get; private set; }
 
+        /// <summary>
+        /// Gets the file name of a transform to be applied
+        /// </summary>
         public string Transform { get; private set; }
 
         #endregion
