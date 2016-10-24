9c216d8d491327f2b9329ea9a88a038d1b4413fd
constructor-igor <igor.zc@gmail.com>
2016-10-25 00:39:39 +0300

    #16 created test ResultServiceTests.CanGetWriterUser

--- src/NUnitEngine/nunit.engine.tests/Services/ResultServiceTests.cs
+++ src/NUnitEngine/nunit.engine.tests/Services/ResultServiceTests.cs
@@ -22,14 +22,12 @@
 // ***********************************************************************
 
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Reflection;
 using NUnit.Framework;
 
 namespace NUnit.Engine.Services.Tests
 {
-    using ResultWriters;
-
     public class ResultServiceTests
     {
         private ResultService _resultService;
@@ -59,13 +57,20 @@ namespace NUnit.Engine.Services.Tests
         [TestCase("nunit3", null, ExpectedResult = "NUnit3XmlResultWriter")]
         //[TestCase("nunit2", null, ExpectedResult = "NUnit2XmlResultWriter")]
         [TestCase("cases", null, ExpectedResult = "TestCaseResultWriter")]
-        [TestCase("user", new object[] { "TextSummary.xslt" }, ExpectedResult = "XmlTransformResultWriter")]
+        //[TestCase("user", new object[] { "TextSummary.xslt" }, ExpectedResult = "XmlTransformResultWriter")]
         public string CanGetWriter(string format, object[] args)
         {
             var writer = _resultService.GetResultWriter(format, args);
 
             Assert.NotNull(writer);
             return writer.GetType().Name;
+        }
+
+        [Test]
+        public void CanGetWriterUser()
+        {
+            string actual = CanGetWriter("user", new object[] { Path.Combine(AssemblyDirectory, "TextSummary.xslt") });
+            Assert.That(actual, Is.EqualTo("XmlTransformResultWriter"));
         }
 
         [Test]
@@ -82,6 +87,17 @@ namespace NUnit.Engine.Services.Tests
             Assert.That(
                 () => _resultService.GetResultWriter("user", null),
                 Throws.TypeOf<ArgumentNullException>());
+        }
+
+        private static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
         }
     }
 }
