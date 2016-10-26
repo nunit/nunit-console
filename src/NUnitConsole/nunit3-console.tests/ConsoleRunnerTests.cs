963b9c72baa0e41be4d39626d11473bdea2a2767
rvignesh89 <rvignesh@outlook.com>
2016-10-26 18:57:51 +0800

    Remove stacktrace and show meaningful message when working directory is inaccessible

---
+++ src/NUnitConsole/nunit3-console.tests/ConsoleRunnerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Common;
+using NUnit.Engine;
+using NUnit.Framework;
+
+namespace NUnit.ConsoleRunner.Tests
+{
+    class ConsoleRunnerTests
+    {
+        [Test]
+        public void ThrowsExceptionWhenWorkingDirectoryIsUnAuthorized()
+        {
+            var options = new ConsoleOptions("tests.dll","-result=results.xml",@"--work=c:\windows\");
+            var engine = TestEngineActivator.CreateInstance();
+            var consoleRunner = new ConsoleRunner(engine, options, new ColorConsoleWriter());
+
+            var exception = Assert.Throws<UnauthorizedAccessException>(()=> { consoleRunner.Execute(); });
+            Assert.That(exception.Message, Is.EqualTo("Error: Unauthorized working directory"));
+        }
+    }
+}
--- src/NUnitConsole/nunit3-console/Program.cs
+++ src/NUnitConsole/nunit3-console/Program.cs
@@ -128,6 +128,11 @@ namespace NUnit.ConsoleRunner
                     {
                         return new ConsoleRunner(engine, Options, OutWriter).Execute();
                     }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        OutWriter.WriteLine(ColorStyle.Error, ex.Message);
+                        return ConsoleRunner.INVALID_ARG;
+                    }
                     catch (TestSelectionParserException ex)
                     {
                         OutWriter.WriteLine(ColorStyle.Error, ex.Message);
