fdeb13080dd4386d4eb75771f8ff132263cde805
rvignesh89 <rvignesh@outlook.com>
2016-10-26 20:30:38 +0800

    use temp directory instead of c:\windows

--- src/NUnitConsole/nunit3-console.tests/ConsoleRunnerTests.cs
+++ src/NUnitConsole/nunit3-console.tests/ConsoleRunnerTests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using NUnit.Common;
 using NUnit.Engine;
 using NUnit.Framework;
@@ -10,12 +13,28 @@ namespace NUnit.ConsoleRunner.Tests
         [Test]
         public void ThrowsExceptionWhenWorkingDirectoryIsUnAuthorized()
         {
-            var options = new ConsoleOptions("tests.dll","-result=results.xml",@"--work=c:\windows\");
-            var engine = TestEngineActivator.CreateInstance();
-            var consoleRunner = new ConsoleRunner(engine, options, new ColorConsoleWriter());
+            var tempDirPath = Directory.GetCurrentDirectory() + "\\" + Path.GetRandomFileName();
+            var directory = Directory.CreateDirectory(tempDirPath);
 
-            var exception = Assert.Throws<UnauthorizedAccessException>(()=> { consoleRunner.Execute(); });
-            Assert.That(exception.Message, Is.EqualTo("Error: Unauthorized working directory"));
+            try
+            {
+                var directorySecurity = new DirectorySecurity();
+                directorySecurity.AddAccessRule(new FileSystemAccessRule(WindowsIdentity.GetCurrent().Name, FileSystemRights.Write, AccessControlType.Deny));
+                directorySecurity.AddAccessRule(new FileSystemAccessRule(WindowsIdentity.GetCurrent().Name, FileSystemRights.DeleteSubdirectoriesAndFiles, AccessControlType.Allow));
+                Directory.SetAccessControl(tempDirPath, directorySecurity);
+
+                var consoleRunner = new ConsoleRunner(
+                    TestEngineActivator.CreateInstance(), 
+                    new ConsoleOptions("tests.dll", "-result=results.xml", 
+                        string.Format(@"--work={0}", directory.FullName)), new ColorConsoleWriter());
+
+                var exception = Assert.Throws<UnauthorizedAccessException>(() => { consoleRunner.Execute(); });
+                Assert.That(exception.Message, Is.EqualTo("Error: Unauthorized working directory"));
+            }
+            finally
+            {
+                directory.Delete(true);
+            }
         }
     }
 }
