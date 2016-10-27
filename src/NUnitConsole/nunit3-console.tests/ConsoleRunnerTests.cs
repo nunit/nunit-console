5cb9209dc5dbe93338be65fbca6e53eac2670e79
rvignesh89 <rvignesh@outlook.com>
2016-10-27 13:23:01 +0800

    Add ConsoleRunnerTest for writability check

---
+++ src/NUnitConsole/nunit3-console.tests/ConsoleRunnerTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using NUnit.Common;
+using NUnit.Engine;
+using NUnit.Engine.Extensibility;
+using NUnit.Engine.Services;
+using NUnit.Framework;
+
+namespace NUnit.ConsoleRunner.Tests
+{
+    class ConsoleRunnerTests
+    {
+        [Test]
+        public void ThrowsNUnitEngineExceptionWhenTestResultsAreNotWriteable()
+        {
+            var testEngine = new TestEngine();
+            testEngine.Services.Add(new FakeResultService());
+
+            var consoleRunner = new ConsoleRunner(testEngine, new ConsoleOptions("mock-assembly.dll"), new ColorConsoleWriter());
+            
+            var ex = Assert.Throws<NUnitEngineException>(() => { consoleRunner.Execute(); });
+            Assert.That(ex.Message, Is.EqualTo("The path specified in --result TestResult.xml could not be written to"));
+        }
+    }
+
+    internal class FakeResultService : Service, IResultService
+    {
+        public string[] Formats
+        {
+            get
+            {
+                return new[] { "nunit3" };
+            }
+        }
+
+        public IResultWriter GetResultWriter(string format, object[] args)
+        {
+            return new FakeResultWriter();
+        }
+    }
+
+    internal class FakeResultWriter : IResultWriter
+    {
+        public void CheckWritability(string outputPath)
+        {
+            throw new UnauthorizedAccessException();
+        }
+
+        public void WriteResultFile(XmlNode resultNode, string outputPath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WriteResultFile(XmlNode resultNode, TextWriter writer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
