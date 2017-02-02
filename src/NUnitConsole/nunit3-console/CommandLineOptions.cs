e905d386727d87234f5d785d3cbb6d9ac4664178
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2017-02-02 10:40:33 +0100

    Remove unused --verbose command line option

--- src/NUnitConsole/nunit3-console/CommandLineOptions.cs
+++ src/NUnitConsole/nunit3-console/CommandLineOptions.cs
@@ -116,8 +116,6 @@ namespace NUnit.Common
 
         public bool NoColor { get; private set; }
 
-        public bool Verbose { get; private set; }
-
         public bool TeamCity { get; private set; }
 
         public string OutFile { get; private set; }
@@ -366,9 +364,6 @@ namespace NUnit.Common
 
             this.Add("nocolor|noc", "Displays console output without color.",
                 v => NoColor = v != null);
-
-            this.Add("verbose|v", "Display additional information as the test runs.",
-                v => Verbose = v != null);
 
             this.Add("help|h", "Display this message and exit.",
                 v => ShowHelp = v != null);
