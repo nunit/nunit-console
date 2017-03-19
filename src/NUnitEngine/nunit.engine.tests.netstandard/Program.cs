428f9efcb202b3d68f7382facf38ea187933e084
Rob Prouse <rob@prouse.org>
2017-03-19 16:19:08 -0400

    Initial .NET Standard unit tests

---
+++ src/NUnitEngine/nunit.engine.tests.netstandard/Program.cs
@@ -0,0 +1,15 @@
+﻿using NUnitLite;
+using System;
+using System.Reflection;
+
+namespace nunit.engine.tests.netstandard
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int result = new TextRunner(typeof(Program).GetTypeInfo().Assembly).Execute(args);
+            return result;
+        }
+    }
+}
