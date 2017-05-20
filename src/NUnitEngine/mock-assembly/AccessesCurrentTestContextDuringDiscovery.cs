e0bf6691be913cb7e5ca921e57f6b79c6ab19c06
jnm2 <me@jnm2.com>
2017-05-19 22:14:18 -0400

    Test for SocketException caused by --explore

---
+++ src/NUnitEngine/mock-assembly/AccessesCurrentTestContextDuringDiscovery.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace NUnit.Tests
+{
+    public class AccessesCurrentTestContextDuringDiscovery
+    {
+        public const int Tests = 2;
+        public const int Suites = 1;
+
+        public static int[] TestCases()
+        {
+            var _ = TestContext.CurrentContext;
+            return new[] { 0 };
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Access_by_TestCaseSource(int arg) { }
+
+        [Test]
+        public void Access_by_ValueSource([ValueSource(nameof(TestCases))] int arg) { }
+    }
+}
