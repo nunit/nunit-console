cee4d2fc0721878d203e5b3bc7bca4c9b3ac115a
jnm2 <me@jnm2.com>
2017-07-09 13:39:54 -0400

    Refactored TcpChannelUtilsTests

--- src/NUnitEngine/nunit.engine.tests/Internal/TcpChannelUtilsTests.cs
+++ src/NUnitEngine/nunit.engine.tests/Internal/TcpChannelUtilsTests.cs
@@ -21,49 +21,83 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
+using System;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
+using NUnit.Engine.Tests.Helpers;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace NUnit.Engine.Internal.Tests
 {
-	[TestFixture]
-	public class TcpChannelUtilsTests
-	{
-		TcpChannel channel1;
-		TcpChannel channel2;
+    [TestFixture]
+    [Parallelizable(ParallelScope.None)] // GetTcpChannel affects the whole AppDomain
+    public class TcpChannelUtilsTests
+    {
+        [Test]
+        public void GetTcpChannelReturnsSameChannelForSameNameDifferentPort()
+        {
+            var first = TcpChannelUtils.GetTcpChannel("test", 1234);
+            using (CleanUpOnDispose(first))
+            {
+                var second = TcpChannelUtils.GetTcpChannel("test", 4321);
+                Assert.That(second, Is.SameAs(first));
+            }
+        }
 
-		[TearDown]
-		public void ReleaseChannels()
-		{
-			channel1.StopListening(null);
-			ChannelServices.UnregisterChannel(channel1);
-			channel2.StopListening(null);
-			ChannelServices.UnregisterChannel(channel2);
-		}
+        [Test]
+        public void GetTcpChannelReturnsSameChannelForSameNameUnspecifiedPorts()
+        {
+            var first = TcpChannelUtils.GetTcpChannel("test", 0);
+            using (CleanUpOnDispose(first))
+            {
+                var second = TcpChannelUtils.GetTcpChannel("test", 0);
+                Assert.That(second, Is.SameAs(first));
+            }
+        }
 
-		[Test]
-		public void CanGetTcpChannelOnSpecifiedPort()
-		{
-			channel1 = TcpChannelUtils.GetTcpChannel( "test", 1234 );
-			Assert.AreEqual( "test", channel1.ChannelName );
-			channel2 = TcpChannelUtils.GetTcpChannel( "test", 4321 );
-			Assert.AreEqual( "test", channel2.ChannelName );
-			Assert.AreEqual( channel1, channel2 );
-			Assert.AreSame( channel1, channel2 );
-			ChannelDataStore cds = (ChannelDataStore)channel1.ChannelData;
-			Assert.AreEqual( "tcp://127.0.0.1:1234", cds.ChannelUris[0] );
-		}
+        [Test]
+        public void GetTcpChannelReturnsChannelWithCorrectName()
+        {
+            var channel = TcpChannelUtils.GetTcpChannel("test", 1234);
+            using (CleanUpOnDispose(channel))
+            {
+                Assert.That(channel, HasChannelName().EqualTo("test"));
+            }
+        }
 
-		[Test]
-		public void CanGetTcpChannelOnUnspecifiedPort()
-		{
-			channel1 = TcpChannelUtils.GetTcpChannel( "test", 0 );
-			Assert.AreEqual( "test", channel1.ChannelName );
-			channel2 = TcpChannelUtils.GetTcpChannel( "test", 0 );
-			Assert.AreEqual( "test", channel2.ChannelName );
-			Assert.AreEqual( channel1, channel2 );
-			Assert.AreSame( channel1, channel2 );
-		}
-	}
+        [Test]
+        public void GetTcpChannelReturnsChannelWithCorrectNameForUnspecifiedPort()
+        {
+            var channel = TcpChannelUtils.GetTcpChannel("test", 0);
+            using (CleanUpOnDispose(channel))
+            {
+                Assert.That(channel, HasChannelName().EqualTo("test"));
+            }
+        }
+
+        [Test]
+        public void GetTcpChannelReturnsChannelWithCorrectURI()
+        {
+            var channel = TcpChannelUtils.GetTcpChannel("test", 1234);
+            using (CleanUpOnDispose(channel))
+            {
+                Assert.That(channel, HasChannelUris().EqualTo(new[] { "tcp://127.0.0.1:1234" }));
+            }
+        }
+
+
+        private static ConstraintExpression HasChannelName() =>
+            Has.Property(nameof(TcpChannel.ChannelName));
+
+        private static ConstraintExpression HasChannelUris() =>
+            Has.Property(nameof(TcpChannel.ChannelData))
+            .With.Property(nameof(ChannelDataStore.ChannelUris));
+
+        private static IDisposable CleanUpOnDispose(IChannelReceiver channel) => On.Dispose(() =>
+        {
+            channel.StopListening(null);
+            ChannelServices.UnregisterChannel(channel);
+        });
+    }
 }
