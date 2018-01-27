1d867e04aa0267e18675c10adc2ca8e328541e1d
Rob Prouse <rob@prouse.org>
2018-01-27 14:40:05 -0500

    Update the version to 3.9 in preparation for the next release

--- src/NUnitEngine/EngineApiVersion.cs
+++ src/NUnitEngine/EngineApiVersion.cs
@@ -29,4 +29,4 @@ using System.Reflection;
 // as new releases of the engine itself are created.
 //
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.8.0.0")]
+[assembly: AssemblyFileVersion("3.9.0.0")]
