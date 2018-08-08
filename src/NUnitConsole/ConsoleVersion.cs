163a8bccf6978d0ab115f8f3cf535c872280be98
jnm2 <me@jnm2.com>
2018-08-08 10:31:43 -0400

    AssemblyInformationalVersion controls the file’s ProductVersion field as a string

--- src/NUnitConsole/ConsoleVersion.cs
+++ src/NUnitConsole/ConsoleVersion.cs
@@ -26,5 +26,5 @@ using System.Reflection;
 //
 // Current version for the NUnit Console
 //
-[assembly: AssemblyVersion("3.9.0.0")]
-[assembly: AssemblyFileVersion("3.9.0.0")]
+[assembly: AssemblyVersion("3.9.0")]
+[assembly: AssemblyInformationalVersion("3.9.0")]
