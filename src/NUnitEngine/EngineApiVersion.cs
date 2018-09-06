a497cb1df4606e1281a5b69bddd2f6034b0b9970
Rob Prouse <rob@prouse.org>
2018-09-05 21:12:09 -0400

    Update the version to 3.10 for ongoing development

--- src/NUnitEngine/EngineApiVersion.cs
+++ src/NUnitEngine/EngineApiVersion.cs
@@ -25,4 +25,4 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("NUnit Engine API")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyInformationalVersion("3.9.0")]
+[assembly: AssemblyInformationalVersion("3.10.0")]
