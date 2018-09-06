9621aaec643e6654fd364c2c9a16a97a2b8eb2e0
Rob Prouse <rob@prouse.org>
2018-09-06 19:19:42 -0400

    Re-add AssemblyFileVersion for the API to fix the MSI install

--- src/NUnitEngine/EngineApiVersion.cs
+++ src/NUnitEngine/EngineApiVersion.cs
@@ -26,3 +26,4 @@ using System.Reflection;
 [assembly: AssemblyProduct("NUnit Engine API")]
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyInformationalVersion("3.9.0")]
+[assembly: AssemblyFileVersion("3.9.0")]
