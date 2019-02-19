f716346dc7b6a31fc3ec269880913c82c0d70a90
Charlie Poole <charliepoole@gmail.com>
2019-02-19 08:53:44 +0100

    Branch for use with testcentric

--- src/NUnitEngine/EngineVersion.cs
+++ src/NUnitEngine/EngineVersion.cs
@@ -25,4 +25,4 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("NUnit Engine")]
 [assembly: AssemblyVersion("3.10.0")]
-[assembly: AssemblyInformationalVersion("3.10.0")]
+[assembly: AssemblyInformationalVersion("3.10.0-tc-00001-dbg")]
