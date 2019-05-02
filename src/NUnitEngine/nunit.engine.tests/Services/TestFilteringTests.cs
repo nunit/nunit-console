f5f29286dbe08b70e1a95113a678c621c21f81d8
Charlie Poole <charliepoole@gmail.com>
2019-05-02 18:13:04 +0100

    Test double-negative filters

--- src/NUnitEngine/nunit.engine.tests/Services/TestFilteringTests.cs
+++ src/NUnitEngine/nunit.engine.tests/Services/TestFilteringTests.cs
@@ -54,14 +54,18 @@ namespace NUnit.Engine.Services.Tests
             _driver.Load(mockAssemblyPath, new Dictionary<string, object>());
         }
 
+        // TODO: Uncomment the "double negative" tests when we are using an updated framework that handles them correctly.
         [TestCase("<filter/>", MockAssembly.Tests)]
         [TestCase("<filter><test>NUnit.Tests.Assemblies.MockTestFixture</test></filter>", MockTestFixture.Tests)]
+        //[TestCase("<filter><not><not><test>NUnit.Tests.Assemblies.MockTestFixture</test></not></not></filter>", MockTestFixture.Tests)]
         [TestCase("<filter><test>NUnit.Tests.Assemblies.MockTestFixture.IgnoreTest</test></filter>", 1)]
+        //[TestCase("<filter><not><not><test>NUnit.Tests.Assemblies.MockTestFixture.IgnoreTest</test></not></not></filter>", 1)]
         [TestCase("<filter><class>NUnit.Tests.Assemblies.MockTestFixture</class></filter>", MockTestFixture.Tests)]
         [TestCase("<filter><name>IgnoreTest</name></filter>", 1)]
         [TestCase("<filter><name>MockTestFixture</name></filter>", MockTestFixture.Tests + NUnit.Tests.TestAssembly.MockTestFixture.Tests)]
         [TestCase("<filter><method>IgnoreTest</method></filter>", 1)]
         [TestCase("<filter><cat>FixtureCategory</cat></filter>", MockTestFixture.Tests)]
+        //[TestCase("<filter><not><not><cat>FixtureCategory</cat></not></not></filter>", MockTestFixture.Tests)]
         public void UsingXml(string filter, int count)
         {
             Assert.That(_driver.CountTestCases(filter), Is.EqualTo(count));
