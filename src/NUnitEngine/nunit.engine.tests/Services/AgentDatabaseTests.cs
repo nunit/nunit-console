11ad95cff51beacd4c73791a591e6158206e41a6
jnm2 <me@jnm2.com>
2019-09-22 23:40:42 -0400

    Update tests

--- src/NUnitEngine/nunit.engine.tests/Services/AgentDatabaseTests.cs
+++ src/NUnitEngine/nunit.engine.tests/Services/AgentDatabaseTests.cs
@@ -1,5 +1,5 @@
 ﻿// ***********************************************************************
-// Copyright (c) 2017 Charlie Poole, Rob Prouse
+// Copyright (c) 2017–2019 Charlie Poole, Rob Prouse
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -30,143 +30,198 @@ using NUnit.Framework;
 
 namespace NUnit.Engine.Services.Tests
 {
-    public class AgentDatabaseTests
+    public static partial class AgentDatabaseTests
     {
-#pragma warning disable 414
-        private static int[] Counts = new int[] { 1, 3, 10 };
-#pragma warning restore 414
+        private static readonly Process DummyProcess = new Process();
+        private static readonly ITestAgent DummyAgent = new DummyTestAgent(Guid.NewGuid());
 
-        const string COUNTS = nameof(Counts);
-
-        AgentDatabase _data;
-        List<Guid> _generatedGuids;
-
-        [SetUp]
-        public void SetUp()
+        [Test]
+        public static void IdCannotBeReused()
         {
-            _data = new AgentDatabase();
-            _generatedGuids = new List<Guid>();
+            var database = new AgentDatabase();
+
+            database.Start(DummyAgent.Id, DummyProcess);
+            Assert.That(() => database.Start(DummyAgent.Id, DummyProcess), Throws.ArgumentException.With.Property("ParamName").EqualTo("agentId"));
+
+            database.Register(DummyAgent);
+            Assert.That(() => database.Start(DummyAgent.Id, DummyProcess), Throws.ArgumentException.With.Property("ParamName").EqualTo("agentId"));
+
+            database.MarkTerminated(DummyAgent.Id);
+            Assert.That(() => database.Start(DummyAgent.Id, DummyProcess), Throws.ArgumentException.With.Property("ParamName").EqualTo("agentId"));
         }
 
         [Test]
-        public void AddData()
+        public static void AgentMustBeStartedBeforeRegistering()
         {
-            AddRecords(5);
+            var database = new AgentDatabase();
 
-            Assert.That(_generatedGuids.Count, Is.EqualTo(5));
-            Assert.That(_data.Count, Is.EqualTo(5));
-
-            var snap = _data.TakeSnapshot();
-            Assert.That(snap.Guids, Is.EquivalentTo(_generatedGuids));
-        }
-
-        [TestCaseSource(COUNTS)]
-        public void AddData_Parallel(int count)
-        {
-            GenerateGuids(count);
-            RunInParallel((g) => AddRecord((Guid)g));
-
-            Assert.That(_generatedGuids.Count, Is.EqualTo(count));
-            Assert.That(_data.Count, Is.EqualTo(count));
-
-            var snap = _data.TakeSnapshot();
-            Assert.That(snap.Guids, Is.EquivalentTo(_generatedGuids));
+            Assert.That(() => database.Register(DummyAgent), Throws.ArgumentException.With.Property("ParamName").EqualTo("agent"));
         }
 
         [Test]
-        public void ReadData()
+        public static void AgentMustNotRegisterTwice()
         {
-            AddRecords(5);
+            var database = new AgentDatabase();
 
-            foreach (var guid in _generatedGuids)
+            database.Start(DummyAgent.Id, DummyProcess);
+            database.Register(DummyAgent);
+            Assert.That(() => database.Register(DummyAgent), Throws.ArgumentException.With.Property("ParamName").EqualTo("agent"));
+        }
+
+        [Test]
+        public static void AgentMustNotRegisterAfterTerminating()
+        {
+            var database = new AgentDatabase();
+
+            database.Start(DummyAgent.Id, DummyProcess);
+            database.MarkTerminated(DummyAgent.Id);
+            Assert.That(() => database.Register(DummyAgent), Throws.ArgumentException.With.Property("ParamName").EqualTo("agent"));
+        }
+
+        [Test]
+        public static void AgentMustBeStartedBeforeTerminating()
+        {
+            var database = new AgentDatabase();
+
+            Assert.That(() => database.MarkTerminated(DummyAgent.Id), Throws.ArgumentException.With.Property("ParamName").EqualTo("agentId"));
+        }
+
+        [Test]
+        public static void AgentIsNotReadyWhenNotStarted()
+        {
+            var database = new AgentDatabase();
+
+            Assert.That(database.IsReady(DummyAgent.Id, out _), Is.False);
+        }
+
+        [Test]
+        public static void AgentIsNotReadyWhenStarted()
+        {
+            var database = new AgentDatabase();
+
+            database.Start(DummyAgent.Id, DummyProcess);
+            Assert.That(database.IsReady(DummyAgent.Id, out _), Is.False);
+        }
+
+        [Test]
+        public static void AgentIsReadyWhenRegistered()
+        {
+            var database = new AgentDatabase();
+
+            database.Start(DummyAgent.Id, DummyProcess);
+            database.Register(DummyAgent);
+            Assert.That(database.IsReady(DummyAgent.Id, out var registeredAgent), Is.True);
+            Assert.That(registeredAgent, Is.SameAs(DummyAgent));
+        }
+
+        [Test]
+        public static void AgentIsNotReadyWhenTerminated()
+        {
+            var database = new AgentDatabase();
+
+            database.Start(DummyAgent.Id, DummyProcess);
+            database.Register(DummyAgent);
+            database.MarkTerminated(DummyAgent.Id);
+            Assert.That(database.IsReady(DummyAgent.Id, out _), Is.False);
+        }
+
+        [Test]
+        public static void AgentIsNotRunningWhenNotStarted()
+        {
+            var database = new AgentDatabase();
+
+            Assert.That(database.IsAgentRunning(DummyAgent.Id, out _), Is.False);
+        }
+
+        [Test]
+        public static void AgentIsRunningWhenStarted()
+        {
+            var database = new AgentDatabase();
+
+            database.Start(DummyAgent.Id, DummyProcess);
+            Assert.That(database.IsAgentRunning(DummyAgent.Id, out var process), Is.True);
+            Assert.That(process, Is.SameAs(DummyProcess));
+        }
+
+        [Test]
+        public static void AgentIsRunningWhenRegistered()
+        {
+            var database = new AgentDatabase();
+
+            database.Start(DummyAgent.Id, DummyProcess);
+            database.Register(DummyAgent);
+            Assert.That(database.IsAgentRunning(DummyAgent.Id, out var process), Is.True);
+            Assert.That(process, Is.SameAs(DummyProcess));
+        }
+
+        [Test]
+        public static void AgentIsNotRunningWhenTerminated()
+        {
+            var database = new AgentDatabase();
+
+            database.Start(DummyAgent.Id, DummyProcess);
+            database.Register(DummyAgent);
+            database.MarkTerminated(DummyAgent.Id);
+            Assert.That(database.IsAgentRunning(DummyAgent.Id, out _), Is.False);
+        }
+
+        [Test]
+        public static void ConcurrentOperationsDoNotCorruptState()
+        {
+            var database = new AgentDatabase();
+
+            RunActionConcurrently(() =>
             {
-                var r = _data[guid];
-                Assert.NotNull(r);
-                Assert.That(r.Id, Is.EqualTo(guid));
-            }
+                for (var i = 0; i < 1000; i++)
+                {
+                    var id = Guid.NewGuid();
+
+                    Assert.That(database.IsAgentRunning(id, out _), Is.False);
+                    Assert.That(database.IsReady(id, out _), Is.False);
+
+                    database.Start(id, DummyProcess);
+                    Assert.That(database.IsAgentRunning(id, out _), Is.True);
+                    Assert.That(database.IsReady(id, out _), Is.False);
+
+                    database.Register(new DummyTestAgent(id));
+                    Assert.That(database.IsAgentRunning(id, out _), Is.True);
+                    Assert.That(database.IsReady(id, out _), Is.True);
+
+                    database.MarkTerminated(id);
+                    Assert.That(database.IsAgentRunning(id, out _), Is.False);
+                    Assert.That(database.IsReady(id, out _), Is.False);
+                }
+            }, threadCount: Environment.ProcessorCount);
         }
 
-        [TestCaseSource(COUNTS)]
-        public void ReadData_Parallel(int count)
-        {
-            AddRecords(count);
-
-            RunInParallel((g) =>
-            {
-                var guid = (Guid)g;
-                var r = _data[guid];
-                Assert.NotNull(r);
-                Assert.That(r.Id, Is.EqualTo(guid));
-            });
-        }
-
-        [Test]
-        public void RemoveData()
-        {
-            AddRecords(5);
-
-            foreach (var guid in _generatedGuids)
-                _data.Remove(guid);
-
-            Assert.That(_data.Count, Is.EqualTo(0));
-        }
-
-        [TestCaseSource(COUNTS)]
-        public void RemoveData_Parallel(int count)
-        {
-            AddRecords(count);
-
-            RunInParallel((g) =>
-            {
-                var guid = (Guid)g;
-                _data.Remove(guid);
-            });
-
-            Assert.That(_data.Count, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void ClearData()
-        {
-            AddRecords(5);
-
-            _data.Clear();
-
-            Assert.That(_data.Count, Is.EqualTo(0));
-        }
-
-        private void GenerateGuids(int count)
-        {
-            while (count-- > 0)
-                _generatedGuids.Add(Guid.NewGuid());
-        }
-
-        private void AddRecord(Guid guid)
-        {
-            _data.AddOrUpdate(AgentRecord.Starting(guid, new Process()));
-        }
-
-        private void AddRecords(int count)
-        {
-            GenerateGuids(count);
-
-            foreach (var guid in _generatedGuids)
-                AddRecord(guid);
-        }
-
-        // Run in parallel for each generated guid
-        private void RunInParallel(ParameterizedThreadStart start)
+        private static void RunActionConcurrently(Action action, int threadCount)
         {
             var threads = new List<Thread>();
+            var exceptions = new List<Exception>();
 
-            for (int i = 0; i < _generatedGuids.Count; i++)
-                threads.Add(new Thread(start));
+            for (var i = 0; i < threadCount; i++)
+            {
+                threads.Add(new Thread(() =>
+                {
+                    try
+                    {
+                        action.Invoke();
+                    }
+                    catch (Exception ex)
+                    {
+                        lock (exceptions)
+                            exceptions.Add(ex);
+                    }
+                }));
+            }
 
-            for (int i = 0; i < _generatedGuids.Count; i++)
-                threads[i].Start(_generatedGuids[i]);
+            foreach (var thread in threads)
+                thread.Start();
 
             foreach (var thread in threads)
                 thread.Join();
+
+            if (exceptions.Count != 0) throw exceptions[0];
         }
     }
 }
