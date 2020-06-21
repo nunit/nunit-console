266a08b036fc125b0106e848a21b00c24633de37
Chris Maddock <chrismaddock@live.co.uk>
2020-06-21 16:00:07 +0100

    Sticking plaster to prevent crash with .NET 2.0 agent encountering .NET 4.0 extension

--- src/NUnitEngine/nunit-agent/Program.cs
+++ src/NUnitEngine/nunit-agent/Program.cs
@@ -67,7 +67,7 @@ namespace NUnit.Agent
                 {
                     debugArgPassed = true;
                 }
-                else if (arg.StartsWith("--trace:"))
+                else if (arg.StartsWith("--trace="))
                 {
                     traceLevel = (InternalTraceLevel)Enum.Parse(typeof(InternalTraceLevel), arg.Substring(8));
                 }
@@ -103,7 +103,7 @@ namespace NUnit.Agent
             };
 
             // Custom Service Initialization
-            engine.Services.Add(new ExtensionService());
+            engine.Services.Add(new ExtensionService(isRunningOnAgent: true));
             engine.Services.Add(new DomainManager());
             engine.Services.Add(new InProcessTestRunnerFactory());
             engine.Services.Add(new DriverService());
