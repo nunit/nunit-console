77fed4ff9b5b2b69c3d591cb15e9bcb956c606a5
Charlie Poole <charliepoole@gmail.com>
2020-06-30 15:55:20 -0700

    Deal with obsolete key if we get one

--- src/NUnitEngine/nunit.engine/Runners/MasterTestRunner.cs
+++ src/NUnitEngine/nunit.engine/Runners/MasterTestRunner.cs
@@ -342,6 +342,17 @@ namespace NUnit.Engine.Runners
                 && _projectService.CanLoadFrom(package.FullName);
         }
 
+        private struct ObsoletePackageSetting
+        {
+            public string OldKey;
+            public string NewKey;
+        }
+
+        private static ObsoletePackageSetting[] ObsoleteSettings = new ObsoletePackageSetting[]
+        {
+            new ObsoletePackageSetting() { OldKey = "RuntimeFramework", NewKey = "RequestedRuntimeFramework"}
+        };
+
         // Any Errors thrown from this method indicate that the client
         // runner is putting invalid values into the package.
         private void ValidatePackageSettings()
@@ -376,6 +387,14 @@ namespace NUnit.Engine.Runners
 
             if (runningInProcess && runAsX86 && IntPtr.Size == 8)
                 throw new NUnitEngineException("Cannot run tests in process - a 32 bit process is required.");
+
+            foreach (var entry in ObsoleteSettings)
+            {
+                var oldKey = entry.OldKey;
+                var newKey = entry.NewKey;
+                if (TestPackage.Settings.ContainsKey(oldKey) && !TestPackage.Settings.ContainsKey(newKey))
+                    TestPackage.Settings[newKey] = TestPackage.Settings[oldKey];
+            }
 #endif
         }
 
