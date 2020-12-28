51e455c31d052cba68950311dd6fd84b9a6f805c
x789 <14997061+x789@users.noreply.github.com>
2020-12-28 12:24:20 +0100

    Added implementation of IFileSystem

---
+++ src/NUnitEngine/nunit.engine.core/Internal/FileSystemAccess/Default/FileSystem.cs
@@ -0,0 +1,65 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FileSystem.cs" company="TillW">
+//   Copyright 2020 TillW. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace NUnit.Engine.Internal.FileSystemAccess.Default
+{
+    using System;
+    using SIO = System.IO;
+
+    /// <summary>
+    /// Default implementation of <see cref="IFileSystem"/> that relies on <see cref="System.IO"/>.
+    /// </summary>
+    internal sealed class FileSystem : IFileSystem
+    {
+        /// <inheritdoc/>
+        public bool Exists(IDirectory directory)
+        {
+            if (directory == null)
+            {
+                throw new ArgumentNullException(nameof(directory));
+            }
+
+            return SIO.Directory.Exists(directory.FullName);
+        }
+
+        /// <inheritdoc/>
+        public bool Exists(IFile file)
+        {
+            if (file == null)
+            {
+                throw new ArgumentNullException(nameof(file));
+            }
+
+            return SIO.File.Exists(file.FullName);
+        }
+
+        /// <inheritdoc/>
+        public IDirectory GetDirectory(string path)
+        {
+            if (SIO.Directory.Exists(path))
+            {
+                return new Directory(path);
+            }
+            else
+            {
+                throw new SIO.DirectoryNotFoundException(string.Format("Directory '{0}' not found.", path));
+            }
+        }
+
+        /// <inheritdoc/>
+        public IFile GetFile(string path)
+        {
+            var directory = SIO.Path.GetDirectoryName(path);
+            if (SIO.Directory.Exists(directory))
+            {
+                return new File(path);
+            }
+            else
+            {
+                throw new SIO.DirectoryNotFoundException(string.Format("Directory '{0}' not found.", directory));
+            }
+        }
+    }
+}
---
+++ src/NUnitEngine/nunit.engine.tests/Internal/FileSystemAccess/Default/FileSystemTests.cs
@@ -0,0 +1,153 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FileSystemTests.cs" company="TillW">
+//   Copyright 2020 TillW. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace NUnit.Engine.Tests.Internal.FileSystemAccess.Default
+{
+    using NUnit.Engine.Internal.FileSystemAccess;
+    using NUnit.Engine.Internal.FileSystemAccess.Default;
+    using NUnit.Framework;
+    using System.Reflection;
+    using SIO = System.IO;
+
+    [TestFixture]
+    public sealed class FileSystemTests
+    {
+        [Test]
+        public void GetDirectory()
+        {
+            var path = SIO.Directory.GetCurrentDirectory();
+            var fileSystem = new FileSystem();
+
+            var actual = fileSystem.GetDirectory(path);
+
+            Assert.That(actual.FullName, Is.EqualTo(path));
+        }
+
+        [Test]
+        public void GetDirectory_DirectoryDoesNotExist()
+        {
+            var path = SIO.Directory.GetCurrentDirectory();
+            while (SIO.Directory.Exists(path))
+            {
+                path += "x";
+            }
+
+            var fileSystem = new FileSystem();
+
+            Assert.That(()=>fileSystem.GetDirectory(path), Throws.InstanceOf<System.IO.DirectoryNotFoundException>());
+        }
+
+        [Test]
+        public void GetFile()
+        {
+            var path = this.GetTestFileLocation();
+            var parent = SIO.Path.GetDirectoryName(path);
+            var fileSystem = new FileSystem();
+
+            var file = fileSystem.GetFile(path);
+
+            Assert.AreEqual(path, file.FullName);
+            Assert.AreEqual(parent, file.Parent.FullName);
+        }
+
+        [Test]
+        public void GetFile_DirectoryDoesNotExist()
+        {
+            var path = SIO.Directory.GetCurrentDirectory();
+            while (SIO.Directory.Exists(path))
+            {
+                path += "x";
+            }
+            path = SIO.Path.Combine(path, "foobar.file");
+
+            var fileSystem = new FileSystem();
+
+            Assert.That(() => fileSystem.GetFile(path), Throws.InstanceOf<System.IO.DirectoryNotFoundException>());
+        }
+
+        [Test]
+        public void Exists_FileExists()
+        {
+            var path = this.GetTestFileLocation();
+            var file = new File(path);
+            var fileSystem = new FileSystem();
+
+            var exists = fileSystem.Exists(file);
+
+            Assert.That(exists, Is.True);
+        }
+
+        [Test]
+        public void Exists_FileDoesnotExist()
+        {
+            var path = this.GetTestFileLocation();
+            while (SIO.File.Exists(path))
+            {
+                path += "x";
+            }
+
+            var file = new File(path);
+            var fileSystem = new FileSystem();
+
+            var exists = fileSystem.Exists(file);
+
+            Assert.That(exists, Is.False);
+        }
+
+        [Test]
+        public void ExistsFileIsNull()
+        {
+            var fileSystem = new FileSystem();
+
+            Assert.That(() => fileSystem.Exists((IFile)null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Exists_DirectoryExists()
+        {
+            var path = SIO.Directory.GetCurrentDirectory();
+            var directory = new Directory(path);
+            var fileSystem = new FileSystem();
+
+            var exists = fileSystem.Exists(directory);
+
+            Assert.That(exists, Is.True);
+        }
+
+        [Test]
+        public void Exists_DirectoryDoesnotExist()
+        {
+            var path = SIO.Directory.GetCurrentDirectory();
+            while (SIO.Directory.Exists(path))
+            {
+                path += "x";
+            }
+
+            var directory = new Directory(path);
+            var fileSystem = new FileSystem();
+
+            var exists = fileSystem.Exists(directory);
+
+            Assert.That(exists, Is.False);
+        }
+
+        [Test]
+        public void Exists_DirectoryIsNull()
+        {
+            var fileSystem = new FileSystem();
+
+            Assert.That(() => fileSystem.Exists((IDirectory)null), Throws.ArgumentNullException);
+        }
+
+        private string GetTestFileLocation()
+        {
+#if NETCOREAPP1_1
+            return Assembly.GetEntryAssembly().Location;
+#else
+            return Assembly.GetAssembly(typeof(FileTests)).Location;
+#endif
+        }
+    }
+}
