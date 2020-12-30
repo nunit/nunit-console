53916b5d4c9c747496c2d7ba423cce93f6818d1d
x789 <14997061+x789@users.noreply.github.com>
2020-12-30 15:35:20 +0100

    Added Any()

---
+++ src/NUnitEngine/nunit.engine.core/IEnumerableExtensions.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IEnumerableExtensions.cs" company="TillW">
+//   Copyright 2020 TillW. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace NUnit.Engine
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Extension methods to get some Linq-feeling in .NET 2.0 projects.
+    /// </summary>
+    internal static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Gets a value indicating whether the enumeration contains at least one item or not.
+        /// </summary>
+        /// <typeparam name="T">The type of objects contained in the enumeration.</typeparam>
+        /// <param name="enumeration">The enumeration.</param>
+        /// <returns><see langword="true"/> if <paramref name="enumeration"/> contains at least one item; otherwhise, <see langword="false"/>.</returns>
+        public static bool Any<T>(this IEnumerable<T> enumeration)
+        {
+            foreach (var _ in enumeration)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
