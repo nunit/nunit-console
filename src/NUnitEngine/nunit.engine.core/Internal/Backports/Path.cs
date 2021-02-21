ff3300d9376b36cdb5c53bf72cc40afa569c3467
x789 <14997061+x789@users.noreply.github.com>
2021-02-21 20:01:28 +0100

    Added an implementation of Path.IsPathFullyQualified(string) that is not available in all targeted frameworks

---
+++ src/NUnitEngine/nunit.engine.core/Internal/Backports/Path.cs
@@ -0,0 +1,56 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Path.cs" company="TillW">
+//   Copyright 2021 TillW. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace NUnit.Engine.Internal.Backports
+{
+    /// <summary>
+    /// Backports of <see cref="System.IO.Path"/> functionality that is only available in newer .NET versions.
+    /// </summary>
+    public static class Path
+    {
+        /// <summary>
+        /// Returns a value that indicates whether the specified file path is absolute or not.
+        /// </summary>
+        /// <param name="path">Path to check</param>
+        /// <returns><see langword="true"/> if <paramref name="path"/> is an absolute or UNC path; otherwhise, false.</returns>
+        /// <remarks>See https://docs.microsoft.com/en-us/dotnet/api/system.io.path.ispathfullyqualified for original implementation.</remarks>
+        public static bool IsPathFullyQualified(string path)
+        {
+            if (RunningOnWindows())
+            {
+                if (path.Length > 2)
+                {
+                    return (IsValidDriveSpecifier(path[0]) && path[1] == ':' && IsDirectorySeparator(path[2]))
+                        || (IsDirectorySeparator(path[0]) && IsDirectorySeparator(path[1]));
+                }
+            }
+            else
+            {
+                if (path.Length >= 2)
+                {
+                    return IsDirectorySeparator(path[0]) && !IsDirectorySeparator(path[1]);
+                }
+            }
+
+            return false;
+        }
+
+        private static bool RunningOnWindows()
+        {
+            return System.IO.Path.DirectorySeparatorChar == '\\';
+        }
+
+        private static bool IsDirectorySeparator(char c)
+        {
+            return c == System.IO.Path.DirectorySeparatorChar || c == System.IO.Path.AltDirectorySeparatorChar;
+        }
+
+        private static bool IsValidDriveSpecifier(char c)
+        {
+            return ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'); 
+        }
+    }
+}
---
+++ src/NUnitEngine/nunit.engine.tests/Internal/Backports/PathTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace NUnit.Engine.Internal.Backports.Tests
+{
+    [TestFixture]
+    public sealed class PathTests
+    {
+        [Platform("win")]
+        [TestCase("c:\\foo\\bar\\", ExpectedResult = true)]
+        [TestCase("c:/foo/bar/", ExpectedResult = true)]
+        [TestCase("c:\\foo\\bar", ExpectedResult = true)]
+        [TestCase("c:/foo/bar", ExpectedResult = true)]
+        [TestCase("c:bar\\", ExpectedResult = false)]
+        [TestCase("c:bar/", ExpectedResult = false)]
+        [TestCase("c:bar", ExpectedResult = false)]
+        [TestCase("ä:\\bar", ExpectedResult = false)]
+        [TestCase("ä://bar", ExpectedResult = false)]
+        [TestCase("\\\\server01\\foo", ExpectedResult = true)]
+        [TestCase("\\server01\\foo", ExpectedResult = false)]
+        [TestCase("c:", ExpectedResult = false)]
+        [TestCase("/foo/bar", ExpectedResult = false)]
+        [TestCase("/", ExpectedResult = false)]
+        [TestCase("\\a\\b", ExpectedResult = false)]
+        public bool IsPathFullyQualified_Windows(string path)
+        {
+            return Path.IsPathFullyQualified(path);
+        }
+
+        [Platform("linux")]
+        [Platform("macos")]
+        [Platform("unix")]
+        [TestCase("/foo/bar", ExpectedResult = true)]
+        [TestCase("/", ExpectedResult = true)]
+        [TestCase("c:\\foo\\bar\\", ExpectedResult = false)]
+        [TestCase("c:/foo/bar/", ExpectedResult = false)]
+        [TestCase("c:\\foo\\bar", ExpectedResult = false)]
+        [TestCase("c:/foo/bar", ExpectedResult = false)]
+        [TestCase("c:bar\\", ExpectedResult = false)]
+        [TestCase("c:bar/", ExpectedResult = false)]
+        [TestCase("c:bar", ExpectedResult = false)]
+        [TestCase("ä:\\bar", ExpectedResult = false)]
+        [TestCase("ä://bar", ExpectedResult = false)]
+        [TestCase("\\\\server01\\foo", ExpectedResult = false)]
+        [TestCase("\\server01\\foo", ExpectedResult = false)]
+        [TestCase("c:", ExpectedResult = false)]
+        [TestCase("\\a\\b", ExpectedResult = false)]
+        public bool IsPathFullyQualified_NonWindows(string path)
+        {
+            return Path.IsPathFullyQualified(path);
+        }
+    }
+}
