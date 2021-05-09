df4c3b01fe8caa168b1c83535b9fb9ca885d55f9
Chris Maddock <chrismaddock@live.co.uk>
2021-05-09 14:33:33 +0100

    Make core engine internals visible to main engine

--- src/NUnitEngine/nunit.engine.core/Properties/AssemblyInfo.cs
+++ src/NUnitEngine/nunit.engine.core/Properties/AssemblyInfo.cs
@@ -13,6 +13,13 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
+[assembly: InternalsVisibleTo("nunit.engine, PublicKey=" +
+    "002400000480000094000000060200000024000052534131000400000100010031eea370b1984b" +
+    "fa6d1ea760e1ca6065cee41a1a279ca234933fe977a096222c0e14f9e5a17d5689305c6d7f1206" +
+    "a85a53c48ca010080799d6eeef61c98abd18767827dc05daea6b6fbd2e868410d9bee5e972a004" +
+    "ddd692dec8fa404ba4591e847a8cf35de21c2d3723bc8d775a66b594adeb967537729fe2a446b5" +
+    "48cd57a6")]
+
 [assembly: InternalsVisibleTo("nunit.engine.tests, PublicKey="+
     "002400000480000094000000060200000024000052534131000400000100010031eea370b1984b" +
     "fa6d1ea760e1ca6065cee41a1a279ca234933fe977a096222c0e14f9e5a17d5689305c6d7f1206"+
