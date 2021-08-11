189146c52c68c8a445477b670919b6007a099ef5
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-11 22:16:35 +0800

    Delete build-mono-docker.ps1

--- build-mono-docker.ps1
+++
@@ -1,8 +0,0 @@
-trap [Management.Automation.CommandNotFoundException] {
-  Write-Error 'Docker cannot be found. Make sure it is installed and added to the path.'
-  Start-Process -FilePath 'https://docs.docker.com/docker-for-windows/install/'
-  continue;
-}
-
-docker build -t nunit-console-build .
-docker run --rm -it -v ${PSScriptRoot}:/nunit-console -w=/nunit-console nunit-console-build bash build.sh $args
