ace8bca389478ae5b1e1fe7b6a5ade8a365ff84b
Charlie Poole <charliepoole@gmail.com>
2021-12-29 17:32:05 -0800

    Let PackageDefinition build the package itself

--- src/NUnitEngine/EngineVersion.cs
+++ src/NUnitEngine/EngineVersion.cs
@@ -3,5 +3,5 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("NUnit Engine")]
-[assembly: AssemblyVersion("3.13.0")]
-[assembly: AssemblyInformationalVersion("3.13.0")]
+[assembly: AssemblyVersion("3.14.0")]
+[assembly: AssemblyInformationalVersion("3.14.0")]
