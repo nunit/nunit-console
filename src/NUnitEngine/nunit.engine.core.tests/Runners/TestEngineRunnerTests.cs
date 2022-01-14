bded5ba3e1d2bb6be82d2ce41368c10f04a6de6b
Mikkel Nylander Bundgaard <mikkelbu@gmail.com>
2022-01-15 00:19:00 +0100

    Remove TODOs

--- src/NUnitEngine/nunit.engine.core.tests/Runners/TestEngineRunnerTests.cs
+++ src/NUnitEngine/nunit.engine.core.tests/Runners/TestEngineRunnerTests.cs
@@ -47,14 +47,10 @@ namespace NUnit.Engine.Runners.Tests
             // Add all services needed by any of our TestEngineRunners
             _services = new ServiceContext();
             _services.Add(new Services.ExtensionService());
-            //_services.Add(new Services.ProjectService()); // TODO
 #if NETFRAMEWORK
             _services.Add(new Services.DomainManager());
-            //_services.Add(new Services.RuntimeFrameworkService()); // TODO
-            //_services.Add(new Services.TestAgency()); // TODO
 #endif
             _services.Add(new Services.DriverService());
-            //_services.Add(new Services.DefaultTestRunnerFactory()); // TODO
             _services.ServiceManager.StartServices();
 
             var mockAssemblyPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "mock-assembly.dll");
