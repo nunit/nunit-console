6c338bb3658daff3be854860bf167d2fdc03263a
Charlie Poole <charliepoole@gmail.com>
2022-01-22 15:42:56 -0800

    Generate Version attributes

--- src/NUnitConsole/ConsoleVersion.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
-
-using System.Reflection;
-
-[assembly: AssemblyVersion("3.14.1")]
-[assembly: AssemblyInformationalVersion("3.14.1")]
