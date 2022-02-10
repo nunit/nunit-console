1cf74fd55d30707c9265304ff266d67386730231
Charlie Poole <charliepoole@gmail.com>
2022-02-10 10:23:07 -0800

    Refactor and simplify TestRunnerFactory tests

--- src/NUnitEngine/nunit.engine.tests/Services/TestRunnerFactoryTests/RunnerResult.cs
+++ src/NUnitEngine/nunit.engine.tests/Services/TestRunnerFactoryTests/RunnerResult.cs
@@ -12,12 +12,32 @@ namespace NUnit.Engine.Tests.Services.TestRunnerFactoryTests
 #if !NETCOREAPP
         public static RunnerResult TestDomainRunner => new RunnerResult(typeof(TestDomainRunner));
         public static RunnerResult ProcessRunner => new RunnerResult(typeof(ProcessRunner));
-        public static RunnerResult MultiRunnerWithTwoSubRunners => new RunnerResult(
-            typeof(MultipleTestProcessRunner),
-            ProcessRunner,
-            ProcessRunner);
+        public static RunnerResult MultipleTestProcessRunner(int numProcesses) =>
+            new RunnerResult(
+                typeof(MultipleTestProcessRunner),
+                GetSubRunners(RunnerResult.ProcessRunner, numProcesses));
+        public static RunnerResult AggregatingTestRunner(int numSubRunners) =>
+            RunnerResult.AggregatingTestRunner(RunnerResult.ProcessRunner, numSubRunners);
 #endif
         public static RunnerResult LocalTestRunner => new RunnerResult(typeof(LocalTestRunner));
+
+        public static RunnerResult AggregatingTestRunner(RunnerResult subRunnerType, int numSubRunners)
+        {
+            return new RunnerResult()
+            {
+                TestRunner = typeof(AggregatingTestRunner),
+                SubRunners = GetSubRunners(subRunnerType, numSubRunners)
+            };
+        }
+
+        public static RunnerResult AggregatingTestRunner(params RunnerResult[] subRunners)
+        {
+            return new RunnerResult()
+            {
+                TestRunner = typeof(AggregatingTestRunner),
+                SubRunners = subRunners
+            };
+        }
 
         public RunnerResult()
         { }
@@ -49,6 +69,15 @@ namespace NUnit.Engine.Tests.Services.TestRunnerFactoryTests
             }
             sb.AppendLine("]");
             return sb.ToString().Trim();
+        }
+
+        private static RunnerResult[] GetSubRunners(RunnerResult subRunner, int count)
+        {
+            var subRunners = new RunnerResult[count];
+            for (int i = 0; i < count; i++)
+                subRunners[i] = subRunner;
+
+            return subRunners;
         }
     }
 }
