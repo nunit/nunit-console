21e5ff52a57b983478c15b9f0a643886544f4ef2
Charlie Poole <charliepoole@gmail.com>
2024-12-28 09:04:02 -0800

    Port issue 1488 to V4

--- src/NUnitEngine/nunit.engine.core/Internal/TestAssemblyLoadContext.cs
+++ src/NUnitEngine/nunit.engine.core/Internal/TestAssemblyLoadContext.cs
@@ -26,6 +26,9 @@ namespace NUnit.Engine.Internal
             _resolver = new TestAssemblyResolver(this, testAssemblyPath);
             _basePath = Path.GetDirectoryName(testAssemblyPath);
             _runtimeResolver = new AssemblyDependencyResolver(testAssemblyPath);
+#if NET8_0_OR_GREATER
+            AppContext.SetData("APP_CONTEXT_BASE_DIRECTORY", _basePath);
+#endif
         }
 
         protected override Assembly Load(AssemblyName name)
