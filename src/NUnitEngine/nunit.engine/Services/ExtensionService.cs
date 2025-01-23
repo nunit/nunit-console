c5e7f0486f23faf712ccaa12b5403a187d9d9144
Charlie Poole <charliepoole@gmail.com>
2025-01-22 22:43:56 -0800

    Eliminate unused Types

--- src/NUnitEngine/nunit.engine/Services/ExtensionService.cs
+++ src/NUnitEngine/nunit.engine/Services/ExtensionService.cs
@@ -5,12 +5,13 @@ using System.Collections.Generic;
 using System.Reflection;
 using NUnit.Engine.Extensibility;
 using NUnit.Engine.Internal;
-using NUnit.Engine.Internal.Backports;
 using NUnit.Engine.Internal.FileSystemAccess;
 using NUnit.Engine.Internal.FileSystemAccess.Default;
 using TestCentric.Metadata;
 
+#if NETFRAMEWORK
 using Backports = NUnit.Engine.Internal.Backports;
+#endif
 #if NET20 || NETSTANDARD2_0
 using Path = NUnit.Engine.Internal.Backports.Path;
 #else
