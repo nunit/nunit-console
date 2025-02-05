c1dc9ec446fa9a3c59319501a5d7538392398fb3
Charlie Poole <charliepoole@gmail.com>
2025-02-04 16:01:30 -0800

    Modify call sequence toagents so all arguments are named

---
+++ src/NUnitEngine/agents/nunit-agent-net462-x86/Net462X86Agent.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
+
+namespace NUnit.Agents
+{
+    public class Net462X86Agent : NUnitAgent<Net462X86Agent>
+    {
+        public static void Main(string[] args) => NUnitAgent<Net462X86Agent>.Execute(args);
+    }
+}
---
+++ src/NUnitEngine/agents/nunit-agent-net80/Net80Agent.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
+
+namespace NUnit.Agents
+{
+    public class Net80Agent : NUnitAgent<Net80Agent>
+    {
+        public static void Main(string[] args) => NUnitAgent<Net80Agent>.Execute(args);
+    }
+}
