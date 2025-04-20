bd0024fb60070a1ad356f366d2d73d7496aa918f
Charlie Poole <charliepoole@gmail.com>
2025-04-20 05:46:30 -0700

    Move ResultSummary to nunit.common

--- src/NUnitCommon/nunit.common/ResultSummary.cs
+++ src/NUnitCommon/nunit.common/ResultSummary.cs
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
 using System;
-using System.Globalization;
 using System.Xml;
-using NUnit.ConsoleRunner.Utilities;
 
-namespace NUnit.ConsoleRunner
+namespace NUnit
 {
     /// <summary>
     /// Summary description for ResultSummary.
